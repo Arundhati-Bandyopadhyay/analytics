ec125c232ffa7c18b892efd5ea5db18fcc517c1e
Arundhati <arundhatibandyopadhyay667@gmail.com>
2024-12-09 01:22:07 +0530

    table added

--- src/app/demotable/demotable.component.ts
+++ src/app/demotable/demotable.component.ts
@@ -4,17 +4,20 @@ import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import {MatIconModule } from '@angular/material/icon'
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { ServiceTsService } from '../service.ts.service';
 import { firstValueFrom } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input'
+import {  MatSortModule} from '@angular/material/sort';
 //import Swal from 'sweetalert2';
 
 @Component({
   selector: 'demotable',
   standalone: true,
-  imports: [MatIconModule,MatTableModule],
+  imports: [MatIconModule,MatTableModule,MatSort,MatPaginatorModule,MatSortModule,MatFormFieldModule,MatInputModule],
   templateUrl: './demotable.component.html',
   styleUrl: './demotable.component.scss'
 })
@@ -24,9 +27,9 @@ export class DemotableComponent {
   addListTableData : any[] = []
   arraydata: any[] = [];
   dataSourceTableData!: MatTableDataSource<any>;
-  displayedColums: string[] = ['Manufacturer','Model'];
+  displayedColums: string[] = ['Manufacturer','Model','four_year_resale_value','Sales_in_thousands'];
 
-  @ViewChild(MatPaginator, { static: true }) paginatorForGemBidDashboard!: MatPaginator
+  @ViewChild(MatPaginator, { static: true }) paginatorFordata!: MatPaginator
   constructor(private apiService: ServiceTsService) {this.fetchData()}
   async fetchData() {
     try {
@@ -34,14 +37,19 @@ export class DemotableComponent {
       this.arraydata = data1;
       this.addListTableData = JSON.parse(JSON.stringify(this.arraydata));
       this.dataSourceTableData = new MatTableDataSource(this.addListTableData);
-      //console.log(this.arraydata.map(item => item.Vehicle_type))
+      this.dataSourceTableData.paginator = this.paginatorFordata
       
       
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceTableData.filter = filterValue.trim().toLowerCase();
+  }
   
 
   
